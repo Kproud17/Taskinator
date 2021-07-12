0331629429bed3ce86e79f05cfb25c708a8c3e06
Kproud17 <kristenmproud@gmail.com>
2021-07-11 21:24:39 -0400

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
 
-buttonEl.addEventListener("click", createTaskHandler); {
+
+};
+
+formEl.addEventListener("submit", createTaskHandler);{
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-console.log(buttonEl);
+//console.log(buttonEl);
 
 var taskItemEl = document.createElement("li");
 taskItemEl.textContent = "hello";
